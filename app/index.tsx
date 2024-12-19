251ae713228c02487bc74c8f381a648eee4a5546
Abed.Matini <abed.matini@ogilvy.co.za>
2024-12-19 16:55:14 +0200

    playing around with ios and android screen conditions. SafeAreaView doesn't work correctly on ios

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
-import { View, Text, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Platform,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../global.css";
 import beachImage from "@/assets/meditation-images/beach.webp";
-import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "@/components/CustomButton";
 
+// Get screen height
+const { height: screenHeight } = Dimensions.get("window");
 const App = () => {
   return (
     <View className="flex-1">
@@ -20,10 +28,26 @@ const App = () => {
           className="flex-1"
           colors={["rgba(0,0,0,0.4)", "rgba(0 ,0,0,0.8)"]}
         >
-          <SafeAreaView className="flex-1 px-1 justify-between">
+          {/* <SafeAreaView className="flex-1 mx-5 my-8 justify-between"> */}
+          <SafeAreaView
+            style={
+              Platform.OS === "ios"
+                ? {
+                    // flex: 1,
+                    marginHorizontal: 20,
+                    marginVertical: screenHeight * 0.37, // 10% of screen height
+                    justifyContent: "space-between",
+                  }
+                : null
+            }
+            className={
+              Platform.OS === "android"
+                ? "flex-1 mx-5 my-8 justify-between"
+                : undefined
+            }
+          >
             <View>
               <Text className="text-center text-white font-bold text-4xl">
-                {" "}
                 Simple Meditation
               </Text>
               <Text className="text-center text-white text-regular text-2xl mt-3">

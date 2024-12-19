1a70342de73360cfffac19987274c668ff1ea9f7
Abed.Matini <abed.matini@ogilvy.co.za>
2024-12-19 11:25:29 +0200

    project cleaned up and nativewind installed. ready to work from scratch

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+// Import your global CSS file
+import "../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-300">
+
+      <Text> Hello World!</Text>
+    </View>
+  );
+}
+

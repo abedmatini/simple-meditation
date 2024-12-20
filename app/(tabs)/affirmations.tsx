218b0a9e7e0608722f772c1a67a04d3a6ad99501
Abed.Matini <abed.matini@ogilvy.co.za>
2024-12-20 16:39:28 +0200

    affirmations.tsx

--- app/(tabs)/affirmations.tsx
+++ app/(tabs)/affirmations.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import AppGradient from "@/components/AppGradient";
+import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import GuidedAffirmationsGallery from "@/components/GuidedAffirmationsGallery";
 
 const affirmations = () => {
   return (
-    <View>
-      <Text>affirmatiosssns</Text>
+    <View className="flex-1">
+      <AppGradient colors={["#2e1f58", "#54426b", "#a790af"]}>
+        <ScrollView showsVerticalScrollIndicators={false}>
+          <Text className="text-zinc-50 text-3xl font-bold">
+            Change your beliefs with affirmations
+          </Text>
+          <View className="mt-5">
+            {AFFIRMATION_GALLERY.map((g) => (
+              <GuidedAffirmationsGallery
+                key={g.title}
+                title={g.title}
+                previews={g.data}
+              />
+            ))}
+          </View>
+        </ScrollView>
+      </AppGradient>
     </View>
   );
 };
